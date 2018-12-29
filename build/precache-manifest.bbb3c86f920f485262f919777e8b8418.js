ef492de71c73a03dede6b838ec31354209a43e49
mcy <mcyelm@163.com>
2018-12-29 10:40:24 +0800

    Initial commit

---
+++ build/precache-manifest.bbb3c86f920f485262f919777e8b8418.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "bba6712455e3c7b642507186d5a666b3",
+    "url": "/static/media/about_01.bba67124.png"
+  },
+  {
+    "revision": "9cf87bdac2cde12757b7",
+    "url": "/static/css/main.69dfbf26.chunk.css"
+  },
+  {
+    "revision": "db12cee6d5560176ed10023d851a80c5",
+    "url": "/static/media/h_z.db12cee6.png"
+  },
+  {
+    "revision": "efe65ea6263709668c00",
+    "url": "/static/js/1.efe65ea6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ff786f023a316efa56553aa70b8a8f17",
+    "url": "/static/media/jone_banner.ff786f02.png"
+  },
+  {
+    "revision": "2696f41ae48521b5ea78303972f19807",
+    "url": "/static/media/j_f_i_head_bg.2696f41a.png"
+  },
+  {
+    "revision": "cfb276021583fbaa6faa2d87f2367203",
+    "url": "/static/media/yins.cfb27602.png"
+  },
+  {
+    "revision": "fa612757a6a85bde2d8dd2e7b93257a2",
+    "url": "/static/media/iconfont.fa612757.eot"
+  },
+  {
+    "revision": "efd5dd770194058bbf2daa5cb73b318a",
+    "url": "/static/media/signupbg.efd5dd77.png"
+  },
+  {
+    "revision": "4c125c446f0dc99526b46355e40e23ec",
+    "url": "/static/media/z01.4c125c44.png"
+  },
+  {
+    "revision": "748afc6fff03586ce3bc873de71b8f3e",
+    "url": "/static/media/iscada02.748afc6f.png"
+  },
+  {
+    "revision": "5c101387a1c2f2ce7a7c7897d6ac82a2",
+    "url": "/static/media/service_banner.5c101387.png"
+  },
+  {
+    "revision": "a763a6eb2486958e57768ad21498b850",
+    "url": "/static/media/service_01.a763a6eb.png"
+  },
+  {
+    "revision": "2b46e6e662c502dfafea60626063e58c",
+    "url": "/static/media/account05.2b46e6e6.png"
+  },
+  {
+    "revision": "9cf87bdac2cde12757b7",
+    "url": "/static/js/main.9cf87bda.chunk.js"
+  },
+  {
+    "revision": "cb0f0866d6f2f2fa9a69ce636f76b573",
+    "url": "/static/media/conn.cb0f0866.png"
+  },
+  {
+    "revision": "959829aa5fda0ffd6590863ba6960afa",
+    "url": "/static/media/serviceCommunity_2.959829aa.jpg"
+  },
+  {
+    "revision": "98be7c566c3833e0722ad1cabe5e4f75",
+    "url": "/static/media/iconfont.98be7c56.ttf"
+  },
+  {
+    "revision": "e52b6b3bb394774a1a2b2c7a233f3798",
+    "url": "/static/media/iconfont.e52b6b3b.svg"
+  },
+  {
+    "revision": "a720f53888e63344715b3ee483e31b31",
+    "url": "/static/media/bg_blue2.a720f538.png"
+  },
+  {
+    "revision": "b260d5f8f026b4f26dc8838885774405",
+    "url": "/static/media/000A.b260d5f8.png"
+  },
+  {
+    "revision": "6a50728c0b8802526ec5fcd34a7b7497",
+    "url": "/static/media/000b.6a50728c.png"
+  },
+  {
+    "revision": "f2b17196fb431525e1b1974de5681879",
+    "url": "/static/media/f_bg.f2b17196.png"
+  },
+  {
+    "revision": "b7060e157746dc209e1c60823d50ed74",
+    "url": "/static/media/f_bg2.b7060e15.png"
+  },
+  {
+    "revision": "0611ad77954734572a8f4c1e99146beb",
+    "url": "/static/media/Carousel-three.0611ad77.png"
+  },
+  {
+    "revision": "daa99c359425e4f676d5c2820ba3e989",
+    "url": "/static/media/Carousel-one.daa99c35.png"
+  },
+  {
+    "revision": "0038af81ff09f5f393d88acc0c2bbb12",
+    "url": "/static/media/Carousel-two.0038af81.png"
+  },
+  {
+    "revision": "1141f1f7b9a605fd60eca77a41efc5a0",
+    "url": "/static/media/service_bgimg.1141f1f7.png"
+  },
+  {
+    "revision": "efe65ea6263709668c00",
+    "url": "/static/css/1.df7ff3c5.chunk.css"
+  },
+  {
+    "revision": "8f12044b326b97017208424a58ac42cb",
+    "url": "/index.html"
+  }
+];
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+const app = express();
+app.use(express.static('build'));
+app.set('views', path.join(__dirname, 'build'));
+app.engine('html', ejs.__express);
+app.set('view engine', 'html');
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+app.listen(8000, () => console.log('listening on port 8000!'));
